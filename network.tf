f959eb177faef58f7162a075a9f344529b494389
saitou-cpi <odeco_de_odeco@yahoo.co.jp>
2024-03-17 13:29:08 +0900

    add endpoint

--- network.tf
+++ network.tf
@@ -14,6 +14,11 @@ resource "aws_vpc" "vpc" {
   }
 }
 
+resource "aws_vpc_endpoint" "s3" {
+  vpc_id       = aws_vpc.vpc.id
+  service_name = "com.amazonaws.${var.region}.s3"
+}
+
 # ---------------------------
 # Subnet
 # ---------------------------
