626d204b1f77d283d03ec869bab659a6d7dfac17
saitou-cpi <odeco_de_odeco@yahoo.co.jp>
2024-03-28 09:05:31 +0900

    ami changed

--- data.tf
+++ data.tf
@@ -3,8 +3,9 @@ data "aws_ami" "amazon_linux_2023" {
   owners      = ["amazon"]
 
   filter {
-    name   = "name"
-    values = ["al2023-ami-2*-kernel-6.1-x86_64"]
+    name = "name"
+    # values = ["al2023-ami-2*-kernel-6.1-x86_64"]
+    values = ["RHEL-9.3.0_HVM-*-x86_64-49-Hourly2-GP3"]
   }
 
 }
--- ec2.tf
+++ ec2.tf
@@ -21,6 +21,12 @@ resource "aws_instance" "master" {
     Name = "${var.project}-master-ec2"
     User = var.user
   }
+  user_data = <<-EOF
+    #!/bin/bash
+    sudo dnf install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
+    sudo systemctl enable amazon-ssm-agent
+    sudo systemctl start amazon-ssm-agent
+  EOF
 
 }
 
